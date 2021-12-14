6675e85c38de60e0879858e026022cf7e255859f
Anshukumarsharma <88080930+Anshukumarsharma@users.noreply.github.com>
2021-12-14 15:30:33 +0530

    Update stuff.py

--- bot/stuff.py
+++ bot/stuff.py
@@ -42,7 +42,7 @@ async def start(event):
 
 async def help(event):
     await event.reply(
-        "** A Quality CompressorQueue**\n\n+This Bot Compress Videos With Negligible Quality Change.\n-Easy to Use\n-Due to Quality Settings Bot Takes Time To Compress.\nSo Be patience Nd Send videos One By One After Completing.\nDont Spam Bot.\n\nJust Forward Video To Start"
+        "** A Quality CompressorQueue**\n\n+This Bot Compress Videos With Negligible Quality Change.\n-Easy to Use\n-Due to Quality Settings Bot Takes Time To Compress.\nSo Be patience Nd Send videos One By One After Completing.\nDont Spam Bot.\n\nJust Forward Video To Start",
     )
 
 
